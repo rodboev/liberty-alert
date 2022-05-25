b4431cb9522347f130825c343dc5c3cf26e73fa0
Rod Boev <rod.boev@gmail.com>
2022-05-25 01:26:41 -0400

    Interim commit debugging FileReader load event completion

--- script.js
+++ script.js
@@ -22,69 +22,75 @@ $(document).ready(function(){
     }
     
     /* show thumbnail after taking photo */    
+    const isMultiUploader = !!$("#mfcf7_zl_multifilecontainer").length
+
     function readURL(input) {
         if (input.files && input.files[0]) {
+            console.log(input.files[0])
+
+            if ($("#mfcf7_zl_multifilecontainer")) {
+                $(input).after('<div class="img-wrapper"><img class="loaded-img"></div>')
+                console.log('created empty image container')
+            }
+
             var reader = new FileReader()
             reader.onload = function (e) {
-                if ($('.loaded-img').length == 0) {
-                    $(input).after('<div class="img-wrapper"><img class="loaded-img"></div>')
+                if ($(input).next() {
+                    console.log('adding img src to container')
+                    $(input).next('.img-wrapper').find('img').attr('src', e.target.result)
                 }
-                $('.loaded-img').attr('src', e.target.result)
+                else {
+                    $('.loaded-img').attr('src', e.target.result)
+                }
             }
-            reader.readAsDataURL(input.files[0])
+            // reader.readAsDataURL(input.files[0])
         }
     }
 
     function fixHeight($el) {
-        console.log('fixing height')
-        $('img.loaded-img').load(function() {
+        $('.loaded-img').load(function() {
             $el.css({
-                'height': $('img.loaded-img').height(),
-                'width': $('img.loaded-img').width(),
+                'height': $('.loaded-img').height(),
+                'width': $('.loaded-img').width(),
             })
         })
     }
 
-    $("input#img-frame").change(function() {
-        readURL(this)
-        fixHeight($('.cameraButton'))
-    })
+    // Find all image input containers:
+    // $("#mfcf7_zl_multifilecontainer").children('.multilinefile-img').find('input#img-frame')
+    // Find just last container:
+    // $("#mfcf7_zl_multifilecontainer").children('.multilinefile-img').last().find('input#img-frame')
 
-    /*
-    const watch =  $('#mfcf7_zl_multifilecontainer)
-    watch.change(function() {
-        const imgFrame = $(this)
-            .children('p.multilinefile-img')
-            .find('input#img-frame')
-        console.log('change: ' + imgFrame[0])
-    })
-    $.when(watch).then((self) => {
-        const imgFrame = $(self).children('p.multilinefile-img').find('input#img-frame')
-        console.log('change: ' + imgFrame[0])
-    })
-    watch.promise().done((self) => {
-        const imgFrame = $(self).children('p.multilinefile-img').find('input#img-frame')
-        console.log('change: ' + imgFrame[0])
-    })
-    */
+    if (isMultiUploader) {
+        $("#mfcf7_zl_multifilecontainer").change(function() {
+            const $inputWrappers = $(this).children('.multilinefile-img')
+            const $lastInput = $inputWrappers.last().find('input#img-frame')
+            const lastInputFilename = $inputWrappers.last()[0].innerText
 
-    $("#mfcf7_zl_multifilecontainer").change(function() {
-        const $input = $(this)
-            .children('p.multilinefile-img')
-            .find('input#img-frame')
-        const inputLength = $input.length - 1
-        const input = $input[inputLength] 
-        readURL(input)
-        fixHeight($('.img-wrapper'));
-    })
+            // console.log('$lastInput (' + $lastInput.length + '): ' + lastInputFilename)
+            // console.log($lastInput[0])
+            // console.log($lastInput[0].files)
+            // console.log($lastInput[0].files[0])
+
+            readURL($lastInput[0])
+            fixHeight($lastInput.next('.img-wrapper'))
+        })
+    }
+    else {
+        $('input#img-frame').change(function() {
+            readURL(this)
+            fixHeight($('.cameraButton'))
+        })
+    }
 
     /* auto format phone number */
+    // https://stackoverflow.com/questions/30058927/format-a-phone-number-as-a-user-types-using-pure-javascript
     const isNumericInput = (event) => {
         const key = event.keyCode;
         return ((key >= 48 && key <= 57) || // Allow number line
             (key >= 96 && key <= 105) // Allow number pad
-        );
-    };
+        )
+    }
     
     const isModifierKey = (event) => {
         const key = event.keyCode;
@@ -96,29 +102,27 @@ $(document).ready(function(){
                 (event.ctrlKey === true || event.metaKey === true) &&
                 (key === 65 || key === 67 || key === 86 || key === 88 || key === 90)
             )
-    };
+    }
     
     const enforceFormat = (event) => {
         // Input must be of a valid number format or a modifier key, and not longer than ten digits
         if(!isNumericInput(event) && !isModifierKey(event)){
-            event.preventDefault();
+            event.preventDefault()
         }
-    };
+    }
     
     const formatToPhone = (event) => {
-        if(isModifierKey(event)) {return;}
+        if(isModifierKey(event)) {return}
+        const input = event.target.value.replace(/\D/g,'').substring(0,10) // First ten digits of input only
+        const areaCode = input.substring(0,3)
+        const middle = input.substring(3,6)
+        const last = input.substring(6,10)
+        if(input.length > 6){event.target.value = `(${areaCode}) ${middle} - ${last}`}
+        else if(input.length > 3){event.target.value = `(${areaCode}) ${middle}`}
+        else if(input.length > 0){event.target.value = `(${areaCode}`}
+    }
     
-        const input = event.target.value.replace(/\D/g,'').substring(0,10); // First ten digits of input only
-        const areaCode = input.substring(0,3);
-        const middle = input.substring(3,6);
-        const last = input.substring(6,10);
-    
-        if(input.length > 6){event.target.value = `(${areaCode}) ${middle} - ${last}`;}
-        else if(input.length > 3){event.target.value = `(${areaCode}) ${middle}`;}
-        else if(input.length > 0){event.target.value = `(${areaCode}`;}
-    };
-    
-    const inputElement = document.querySelector('input[type="tel"');
-    inputElement.addEventListener('keydown',enforceFormat);
-    inputElement.addEventListener('keyup',formatToPhone);
-});
+    const inputElement = document.querySelector('input[type="tel"')
+    inputElement.addEventListener('keydown',enforceFormat)
+    inputElement.addEventListener('keyup',formatToPhone)
+})
